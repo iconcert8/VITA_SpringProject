65be43d8b5308715e0ee19e3158b8c83555ad76d
tsedek <whiteus@gmail.com>
2019-07-30 21:47:40 +0900

    190730-3 messenger is working..

--- vita/src/main/webapp/resources/js/messengerWebSocket.js
+++ vita/src/main/webapp/resources/js/messengerWebSocket.js
@@ -1,16 +1,42 @@
 
 var messengerWs;
+var messengerAnalyzer;
+var notiSelect;
 var messengerWebSocket = function () {
     messengerws = new WebSocket("ws://localhost:8081/messenger/websocket");
 
+    var authUserId = $('#authUserId').val();
     messengerws.onopen = function () {
         console.log("[!]info: messenger connection opened");
+
+        if(authUserId) {
+            $('#messengerNotiList').empty();
+            $('#messengerNotiList').append(`<button class="dropdown-item" onclick="location.href='/messenger'">메신저창으로 이동</button>`);
+            var count = 0;
+            messengerService.getList(function (result) {
+                $.each(result, function (i, item) {
+                    $('#messengerNotiList').append(template.notiMessengerList(item));
+                    count += item.readless;
+                    console.log(count);
+                });
+                $('#messengerToalCnt').text(count);
+            });
+            
+        }
     }
 
     messengerws.onmessage = function (event) {
         var responseData = event.data;
         var data = JSON.parse(responseData);
-        messengerAnalyzer(data);
+
+        if(data.type === 'noti') {
+
+        } if(messengerAnalyzer) {
+        	messengerAnalyzer(data);
+        } else {
+        	
+        }
+       
     }
 
     messengerws.onclose = function (event) {
